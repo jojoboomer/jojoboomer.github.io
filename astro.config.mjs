2bf2a877e3a11ab57400346683d6771c1335c754
jjlabrada <jjlabranate@gmail.com>
2024-11-11 17:07:02 -0500

    main changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://jojosafk.github.io',
-  base: '/',
+  site: "https://jojosafk.github.io",
+  base: "/",
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,23 +5,29 @@ export default {
   theme: {
     extend: {
       colors: {
+        //Aux
         black: "#000000",
         gray: "#434343",
-        // 'darkmode': "#1b1818",
-        // 'primary-900': "#601d00",
-        // 'primary-700': "#af4011",
-        // 'primary': "#CC5725",
-        // 'primary-300': "#e57443",
-        // 'primary-100': "#ff986c",
-        'darkmode': "#1A191D",
-        'lightmode': "#fff",
-        'primary-900': "#001B87",
-        'primary-700': "#1431A9",
-        'primary': "#2341B8",
-        'primary-300': "#3958D6",
-        'primary-100': "#5A7BFF",
-
-        glass:"rgba(255, 255, 255, 0.15)",
+        gray: "#494D58",
+        //social colors
+        linkedin: "#0073B2",
+        github: "#000000",
+        email: "#D75448",
+        x: "#000000",
+        //Background
+        darkmode: "#0D1717",
+        lightmode: "#F0F0F0",
+        glass: "rgba(255, 255, 255, 0.15)",
+        //Brands
+        primary: "#E7473C",
+        "primary-100": "#FF938B",
+        "primary-300": "#FC6D63",
+        "primary-500": "#E7473C",
+        "primary-700": "#BB251A",
+        "primary-900": "#890F06",
+        "azul-oxford": "hsla(221, 51%, 16%, 1)",
+        // azul oscuro clásico (#0A1828), turquesa (#178582), dorado (#BFA181)
+        //humo blanco (#F0F0F0), rojo brillante (#E7473C)
       },
       fontSize: {
         52: "52px",
@@ -32,25 +38,26 @@ export default {
       },
       backgroundImage: {
         noise: "url('/img/hero-pattern.svg')",
+        gradient: "radial",
       },
-      gridTemplateRows:{
-        card:'repeat(4, auto)'
+      gridTemplateRows: {
+        card: "repeat(4, auto)",
       },
-      gridTemplateCol:{
-        card:'60% 40%'
+      gridTemplateCol: {
+        card: "60% 40%",
       },
       lineHeight: {
-        subhead: '1.1',
-        head: '1.3',
-        text: '1.5',
+        subhead: "1.1",
+        head: "1.3",
+        text: "1.5",
       },
       letterSpacing: {
-        head: '-0.03em',
-        text: '0.03em',
+        head: "-0.03em",
+        text: "0.03em",
       },
       boxShadow: {
-        'top': '0px -10px 15px -3px  rgba(0,0,0,0.2)',
-      }
+        top: "0px -10px 15px -3px  rgba(0,0,0,0.2)",
+      },
     },
   },
   plugins: [],
